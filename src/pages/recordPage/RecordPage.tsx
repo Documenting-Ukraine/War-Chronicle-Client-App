3dc0e2ffae1063481cab776861ce272b3a5eafeb
Arky Asmal <arkyasmal@gmail.com>
2022-11-07 00:41:43 -0500

    remove test data from record page

--- src/pages/recordPage/RecordPage.tsx
+++ src/pages/recordPage/RecordPage.tsx
@@ -4,90 +4,44 @@ import { useRealmApp } from "../../realm/RealmApp";
 import LoadingMessage from "../utilityComponents/loadingMessage/LoadingMessage";
 import PageBanner from "../utilityComponents/pageBanner/PageBanner";
 import RecordPageWrapper from "./utilities/RecordPageWrapper";
-import { RecordSubmissionType } from "../../types";
-import { GeneralRecordType } from "../../types/dataTypes/GeneralRecordType";
 import InternationalResponsePage from "./recordTypes/InternationalResponse/InternationalResponse";
 import WarCrimesPage from "./recordTypes/WarCrimes/WarCrimes";
 import MediaAndDisinformationPage from "./recordTypes/MediaAndDisinformation/MediaAndDisinformation";
 import ProtestsAbroadPage from "./recordTypes/ProtestsAbroad/ProtestsAbroad";
 import RefugeesAndIdpsPage from "./recordTypes/RefugeesAndIDPs/RefugeesAndIDPs";
 import RussiaPage from "./recordTypes/Russia/Russia";
-
-// import RecordContentPage from "./RecordContentPage";
 const RecordPage = () => {
   const namespace = "record-pg";
   const params = useParams();
   const app = useRealmApp();
   const recordId = params.id ? params.id : "";
-  //const { data, err, loading } = useFetchRecordData({ app, recordId });
-  const { err } = useFetchRecordData({ app, recordId });
-  const loading: "fullfilled" | "loading" | "failed" = "fullfilled";
-  const data: RecordSubmissionType = {
-    _id: "38294793",
-    record_title: "Hello",
-    record_creation_date: new Date(),
-    media: { images: [], videos: [] },
-    description: "Hello",
-    evidence: [{ _id: "4234f2r3", url: "Hello", description: "derfwewf" }],
-    contributors: [
-      {
-        _id: "32434234",
-        first_name: "Arky",
-        last_name: "Asmal",
-        date_first_edited: new Date(),
-      },
-    ],
-    //event type
-    // address: {
-    //   oblast: "Kyiv",
-    //   city: "Kyiv",
-    //   latitude: "14412",
-    //   longitude: "124124",
-    // },
-    // date_first_published: new Date(),
-    // date_event_occurred: new Date(),
-    //end of event type
-    record_type: "Russia",
-    // date_first_published: new Date(),
-    notes: "hello the world ends",
-    russian_record_type: "Corporation Responses",
-    corporation_name: "Hello",
-    corporation_industry: "hello",
-    date_of_first_response: new Date(),
-    date_of_most_recent_response: new Date(),
-    russian_record_response_type: "Removal of Assets",
-    russian_record_custom_response_type: "Cool",
-    donation_valuation: 100000,
-    // refugees_and_idps_type: "IDPs",
-    // total_num_of_idps: 1000
-  };
+  const { data, err, loading } = useFetchRecordData({ app, recordId });
   let formTypeDataInputs = <></>;
   switch (data?.record_type) {
-    // case "International Response":
-    //   formTypeDataInputs = <InternationalResponsePage data={data} />;
-    //   break;
-    // case "War Crimes":
-    //   formTypeDataInputs = <WarCrimesPage data={data} />;
-    //   break;
-    // case "Media And Disinformation":
-    //   formTypeDataInputs = <MediaAndDisinformationPage data={data} />;
-    //   break;
-    // case "Protests Abroad":
-    //   formTypeDataInputs = <ProtestsAbroadPage data={data} />;
-    //   break;
-    // case "Refugees And IDPs":
-    //   formTypeDataInputs = <RefugeesAndIdpsPage data={data} />;
-    //   break;
+    case "International Response":
+      formTypeDataInputs = <InternationalResponsePage data={data} />;
+      break;
+    case "War Crimes":
+      formTypeDataInputs = <WarCrimesPage data={data} />;
+      break;
+    case "Media And Disinformation":
+      formTypeDataInputs = <MediaAndDisinformationPage data={data} />;
+      break;
+    case "Protests Abroad":
+      formTypeDataInputs = <ProtestsAbroadPage data={data} />;
+      break;
+    case "Refugees And IDPs":
+      formTypeDataInputs = <RefugeesAndIdpsPage data={data} />;
+      break;
     case "Russia":
       formTypeDataInputs = <RussiaPage data={data} />;
       break;
-    // default:
-    //   break;
+    default:
+      break;
   }
-  console.log(data);
   return (
     <div id={`${namespace}-container`}>
-      {/* {loading === "loading" && (
+      {loading === "loading" && (
         <div id={`${namespace}-loading-container`}>
           <LoadingMessage text={"Fetching you record results"} />
         </div>
@@ -97,7 +51,7 @@ const RecordPage = () => {
           bannerMessage={err.message}
           className={`${namespace}-err-banner`}
         />
-      )} */}
+      )}
       {loading === "fullfilled" && data && (
         <RecordPageWrapper data={data}>
           <>{formTypeDataInputs}</>
